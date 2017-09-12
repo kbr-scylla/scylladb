3dda21eb49a05caf4e87c9e2b5a73d7b1605e6c4
Calle Wilund <calle@scylladb.com>
2017-09-12 11:03:11 +0000

    scylla-enterprise: openssl symmetric key wrapper
    by-name created symmetric key that can provide all
    tyeps supported by openssl libcrypt/evp, and accepts 
    algo names dse style.

---
+++ ent/encryption/symmetric_key.hh
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2018 ScyllaDB
+ *
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * See the LICENSE.PROPRIETARY file in the top-level directory for licensing information.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "../../stdx.hh"
+#include "../../bytes.hh"
+
+// forward declare openssl evp.
+extern "C" {
+struct evp_cipher_ctx_st;
+}
+
+namespace encryption {
+
+struct key_info {
+    sstring alg;
+    unsigned len;
+};
+
+bool operator==(const key_info& k1, const key_info& k2);
+bool operator!=(const key_info& k1, const key_info& k2);
+
+struct key_info_hash {
+    size_t operator()(const key_info& e) const;
+};
+
+class symmetric_key {
+    std::unique_ptr<evp_cipher_ctx_st, void (*)(evp_cipher_ctx_st*)> _ctxt;
+    key_info _info;
+    bytes _key;
+    unsigned _iv_len = 0;
+    unsigned _block_size = 0;
+    bool _padding = true;
+
+    operator evp_cipher_ctx_st *() const {
+        return _ctxt.get();
+    }
+
+    void generate_iv_impl(uint8_t* dst, size_t) const;
+    size_t decrypt_impl(const uint8_t* input, size_t input_len, uint8_t* output,
+                    size_t output_len, const uint8_t* iv) const;
+    size_t encrypt_impl(const uint8_t* input, size_t input_len, uint8_t* output,
+                    size_t output_len, const uint8_t* iv) const;
+
+public:
+    symmetric_key(const key_info& info, const bytes& key = { });
+
+    const key_info& info() const {
+        return _info;
+    }
+    const bytes& key() const {
+        return _key;
+    }
+    size_t iv_len() const {
+        return _iv_len;
+    }
+    size_t block_size() const {
+        return _block_size;
+    }
+
+    /**
+     * Write a random IV to dst. Must be iv_len() sized or larger
+     */
+    template<typename T>
+    void generate_iv(T* dst, size_t s) const {
+        static_assert(sizeof(T) == sizeof(uint8_t) && std::is_integral_v<T>);
+        generate_iv_impl(reinterpret_cast<uint8_t *>(dst), s);
+    }
+
+    // returns minimal buffer size required to encrypt n bytes. I.e.
+    // block alignment
+    size_t encrypted_size(size_t n) const;
+
+    template<typename T, typename V, typename I = char>
+    size_t decrypt(const T* input, size_t input_len, V* output,
+                    size_t output_len, const I* iv = nullptr) const {
+        static_assert(sizeof(T) == sizeof(uint8_t) && std::is_integral_v<T>);
+        return decrypt_impl(reinterpret_cast<const uint8_t*>(input), input_len,
+                        reinterpret_cast<uint8_t *>(output), output_len,
+                        reinterpret_cast<const uint8_t*>(iv));
+    }
+    template<typename T, typename V, typename I = char>
+    size_t encrypt(const T* input, size_t input_len, V* output,
+                    size_t output_len, const I* iv = nullptr) const {
+        static_assert(sizeof(T) == sizeof(uint8_t) && std::is_integral_v<T>);
+        return encrypt_impl(reinterpret_cast<const uint8_t*>(input), input_len,
+                        reinterpret_cast<uint8_t *>(output), output_len,
+                        reinterpret_cast<const uint8_t*>(iv));
+    }
+
+    enum class mode {
+        decrypt, encrypt,
+    };
+    template<typename T, typename V, typename I = char>
+    void transform_unpadded(mode m, const T* input, size_t input_len, V* output,
+                    const I* iv = nullptr) const {
+        static_assert(sizeof(T) == sizeof(uint8_t) && std::is_integral_v<T>);
+        return transform_unpadded_impl(reinterpret_cast<const uint8_t*>(input),
+                        input_len, reinterpret_cast<uint8_t *>(output),
+                        reinterpret_cast<const uint8_t*>(iv), m);
+    }
+    template<typename T, typename V, typename I = char>
+    void encrypt_unpadded(const T* input, size_t input_len, V* output,
+                    const I* iv = nullptr) const {
+        static_assert(sizeof(T) == sizeof(uint8_t) && std::is_integral_v<T>);
+        return transform_unpadded_impl(reinterpret_cast<const uint8_t*>(input),
+                        input_len, reinterpret_cast<uint8_t *>(output),
+                        reinterpret_cast<const uint8_t*>(iv), mode::encrypt);
+    }
+    template<typename T, typename V, typename I = char>
+    void decrypt_unpadded(const T* input, size_t input_len, V* output,
+                    const I* iv = nullptr) const {
+        static_assert(sizeof(T) == sizeof(uint8_t) && std::is_integral_v<T>);
+        return transform_unpadded_impl(reinterpret_cast<const uint8_t*>(input),
+                        input_len, reinterpret_cast<uint8_t *>(output),
+                        reinterpret_cast<const uint8_t*>(iv), mode::decrypt);
+    }
+
+private:
+    void transform_unpadded_impl(const uint8_t* input, size_t input_len,
+                    uint8_t* output, const uint8_t* iv, mode) const;
+};
+
+}
