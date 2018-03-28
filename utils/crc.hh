vladzcloudius/scylla

utils::crc32: add power64 crc32 HW accelerated implementation
Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

utils/crc.hh
@@ -22,6 +22,7 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 
 #if defined(__x86_64__) || defined(__i386__)
 #include <smmintrin.h>
@@ -98,6 +99,17 @@ public:
         if (size >= 1) {
             process(*in);
         }
+    }
+#elif defined(__PPC64__)
+    uint32_t crc32_vpmsum(uint32_t crc, const uint8_t* p, size_t len);
+
+    template <class T>
+    void process(T in) {
+        static_assert(std::is_integral<T>::value, "T must be integral type.");
+        _r = crc32_vpmsum(_r, reinterpret_cast<const uint8_t*>(&in), sizeof(T));
+    }
+    void process(const uint8_t* in, size_t size) {
+        _r = crc32_vpmsum(_r, in, size);
     }
 #else
     // On non-x86 platforms use the zlib implementation of crc32.

kbr-scylla/scylladb

Based on the work of Anton Blanchard <anton@au.ibm.com>, IBM that may be found
here: https://github.com/antonblanchard/crc32-vpmsum
(cherry picked from commit 0145ae2b4b54d99e57dee2901ed7682a6b591ee4)

utils/crc.hh
@@ -6,11 +6,15 @@
  * This file is part of Scylla.
  *
  * See the LICENSE.PROPRIETARY file in the top-level directory for licensing information.
+ *
+ * A crc32 calculation for __PPC64__ uses the code from https://github.com/antonblanchard/crc32-vpmsum
+ * written by Anton Blanchard <anton@au.ibm.com>, IBM
  */
 
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 #include <seastar/net/byteorder.hh>
 
 #if defined(__x86_64__) || defined(__i386__)
@@ -95,6 +99,32 @@ public:
         if (size >= 1) {
             process_le(*in);
         }
+    }
+#elif defined(__PPC64__)
+    uint32_t crc32_vpmsum(uint32_t crc, const uint8_t* p, size_t len);
+
+    template <class T>
+    void process_le(T in) {
+        static_assert(std::is_integral<T>::value, "T must be integral type.");
+#if defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        switch (sizeof(T)) {
+        case 2: in = __builtin_bswap16(in); break;
+        case 4: in = __builtin_bswap32(in); break;
+        case 8: in = __builtin_bswap64(in); break;
+        }
+#endif
+        _r = crc32_vpmsum(_r, reinterpret_cast<const uint8_t*>(&in), sizeof(T));
+    }
+
+    template <class T>
+    void process_be(T in) {
+        static_assert(std::is_integral<T>::value, "T must be integral type.");
+        in = seastar::net::hton(in);
+        _r = crc32_vpmsum(_r, reinterpret_cast<const uint8_t*>(&in), sizeof(T));
+    }
+
+    void process(const uint8_t* in, size_t size) {
+        _r = crc32_vpmsum(_r, in, size);
     }
 #else
     template <class T>