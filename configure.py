e9af89275be31c4c8be5e471878a44940efad607
Gleb Natapov <gleb@scylladb.com>
2018-06-10 15:30:17 +0300

    In memory file implementation
    This patch introduce file implementation that stores data in dedicated
    memory as opposite of putting it in some kind of non-volatile memory.
    Obviously after restart all files will be gone.

--- configure.py
+++ configure.py
@@ -637,6 +637,7 @@ scylla_core = (['database.cc',
                  'ent/encryption/encrypted_file_impl.cc',
                  'ent/encryption/kmip_host.cc',
                  'ent/encryption/kmip_key_provider.cc',
+                 'in-memory-file-impl.cc',
                  ]
                 + [Antlr3Grammar('cql3/Cql.g')]
                 + [Thrift('interface/cassandra.thrift', 'Cassandra')]
