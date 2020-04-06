kbr-scylla/scylladb

commit 7dc7ef0de097f09b496834a47f0055ee435459a3

audit: add query string to audit_info
It will be part of audit message.

Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

cql3/Cql.g
@@ -583,7 +583,7 @@ batchStatement returns [shared_ptr<cql3::statements::raw::batch_statement> expr]
     : K_BEGIN
       ( K_UNLOGGED { type = btype::UNLOGGED; } | K_COUNTER { type = btype::COUNTER; } )?
       K_BATCH ( usingClause[attrs] )?
-          ( s=batchStatementObjective ';'? { statements.push_back(std::move(s)); } )*
+          ( s=batchStatementObjective ';'? { $s.statement->add_raw(sstring{$s.text}); statements.push_back(std::move($s.statement)); } )*
       K_APPLY K_BATCH
       {
           $expr = ::make_shared<cql3::statements::raw::batch_statement>(type, std::move(attrs), std::move(statements));

audit/audit.hh
@@ -30,12 +30,16 @@ class audit_info final {
     statement_category _category;
     sstring _keyspace;
     sstring _table;
+    sstring _query;
 public:
     audit_info(statement_category cat, sstring keyspace, sstring table)
         : _category(cat)
         , _keyspace(std::move(keyspace))
         , _table(std::move(table))
     { }
+    void set_query_string(const std::experimental::string_view& query_string) {
+        _query = query_string.to_string();
+    }
 };
 
 using audit_info_ptr = std::unique_ptr<audit_info>;

cql3/query_processor.cc
@@ -315,7 +315,12 @@ query_processor::get_statement(const sstring_view& query, const service::client_
         cf_stmt->prepare_keyspace(client_state);
     }
     ++_stats.prepare_invocations;
-    return statement->prepare(_db.local(), _cql_stats);
+    auto res = statement->prepare(_db.local(), _cql_stats);
+    auto audit_info = res->statement->get_audit_info();
+    if (audit_info) {
+        audit_info->set_query_string(query);
+    }
+    return std::move(res);
 }
 
 ::shared_ptr<raw::parsed_statement>

cql3/statements/batch_statement.cc
@@ -427,6 +427,10 @@ batch_statement::prepare(database& db, cql_stats& stats) {
             have_multiple_cfs = first_ks.value() != parsed->keyspace() || first_cf.value() != parsed->column_family();
         }
         statements.push_back(parsed->prepare(db, bound_names, stats));
+        auto audit_info = statements.back()->get_audit_info();
+        if (audit_info) {
+            audit_info->set_query_string(parsed->get_raw_cql());
+        }
     }
 
     auto&& prep_attrs = _attrs->prepare(db, "[batch]", "[batch]");

cql3/statements/raw/modification_statement.hh
@@ -71,6 +71,7 @@ class modification_statement;
 namespace raw {
 
 class modification_statement : public cf_statement {
+    sstring _raw_cql;
 public:
     using conditions_vector = std::vector<std::pair<::shared_ptr<column_identifier::raw>, ::shared_ptr<column_condition::raw>>>;
 protected:
@@ -85,6 +86,8 @@ protected:
 public:
     virtual std::unique_ptr<prepared> prepare(database& db, cql_stats& stats) override;
     ::shared_ptr<cql3::statements::modification_statement> prepare(database& db, ::shared_ptr<variable_specifications> bound_names, cql_stats& stats);
+    void add_raw(sstring&& raw) { _raw_cql = std::move(raw); }
+    const sstring& get_raw_cql() const { return _raw_cql; }
 protected:
     virtual ::shared_ptr<cql3::statements::modification_statement> prepare_internal(database& db, schema_ptr schema,
         ::shared_ptr<variable_specifications> bound_names, std::unique_ptr<attributes> attrs, cql_stats& stats) = 0;

commit 3b3761ce65174709b8bddd533ac3df9b1af50555

cql: restore properly setting raw query string for batch statements
This patch fixes regression introduced by https://github.com/scylladb/scylla-enterprise/commit/7dc7ef0de097f09b496834a47f0055ee435459a3.

In order to use predefined `.text` attribute ANTLR has to generate
a utility class to hold multiple return values, but only generates
a copy assignment operator, which fails to compile for
std::unique_ptr.

Wrap unique_ptr from the batchStatementObjective into lw_shared_ptr
and unwrap it again in batchStatement to work around the antlr issue.

Tests: unit(dev), dtest-enterprise(audit_tests.py)
Fixes: #1307

Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
Message-Id: <20200406131107.77917-1-pa.solodovnikov@scylladb.com>

cql3/Cql.g
@@ -646,14 +646,19 @@ batchStatement returns [std::unique_ptr<cql3::statements::raw::batch_statement> 
     : K_BEGIN
       ( K_UNLOGGED { type = btype::UNLOGGED; } | K_COUNTER { type = btype::COUNTER; } )?
       K_BATCH ( usingClause[attrs] )?
-          ( s=batchStatementObjective ';'? { statements.push_back(std::move(s)); } )*
+          ( s=batchStatementObjective ';'?
+              {
+                  auto&& stmt = *$s.statement;
+                  stmt->add_raw(sstring{$s.text});
+                  statements.push_back(std::move(stmt));
+              } )*
       K_APPLY K_BATCH
       {
           $expr = std::make_unique<cql3::statements::raw::batch_statement>(type, std::move(attrs), std::move(statements));
       }
     ;
 
-batchStatementObjective returns [std::unique_ptr<cql3::statements::raw::modification_statement> statement]
+batchStatementObjective returns [::lw_shared_ptr<std::unique_ptr<cql3::statements::raw::modification_statement>> statement]
     : i=insertStatement  { $statement = std::move(i); }
     | u=updateStatement  { $statement = std::move(u); }
     | d=deleteStatement  { $statement = std::move(d); }