d4d24c89ac5207b13357ede5f09695a6d97fa613
Avi Kivity <avi@scylladb.com>
2020-06-16 19:59:31 +0300

    scripts: license: avoid touching binary and sstable files
    The relicensing script uses awk, which adds a trailing newline
    if it thinks it is needed. This isn't good for binary files.
    
    Fix by:
     - skipping files known to be sstable files (ending with .db or .crc32)
     - checking the mime type of files and skipping non-text files
    
    Fixes #1392.

--- scripts/license.sh
+++ scripts/license.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#
 git ls-files \
     | xargs ls -Fd \
+    | grep -vE '\.db$|\.crc32$' \
     | grep -v '^scripts/license.awk$' \
     | grep -v '[/@]$' \
     | sed 's/\*$//' \
+    | xargs file --mime-type --no-pad  | sed -n '/: text/ { s/: text.*$//; p}' \
     | xargs awk -i inplace -f scripts/license.awk
