13054042dadfb2e12faaf439ee1fef2b529fd33e
Calle Wilund <calle@scylladb.com>
2021-09-20 12:18:01 +0000

    symmetric_key: Verify the padding string input (if any)
    Fixes #1934
    
    OpenSSL does only support one form of padding (indeed, there pretty much
    only exists one), so we don't really verify the actual string used there,
    just that it exists. This is probably not the greatest, and we should
    raise errors if user tries to input something non-sequitor.
    
    Note that nothing (empty) is of course still valid, though perhaps
    discouraged.
    
    Adds a few tests for this and general input info == output info.

--- test/boost/symmetric_key_test.cc
+++ test/boost/symmetric_key_test.cc
@@ -99,3 +99,91 @@ SEASTAR_TEST_CASE(test_cipher_types) {
     }
     return make_ready_future<>();
 }
+
+// OpenSSL only supports one form of padding. We used to just allow
+// non-empty string -> pkcs5/pcks7. We now instead verify this to be 
+// within the "sane" limits, i.e. pkcs, pkcs5 or pkcs7. 
+// Check an non-exhaustive set of invalid padding options and verify 
+// we get an exception as expected.
+// See below for test for valid strings.
+SEASTAR_TEST_CASE(test_invalid_padding_options) {
+    static const std::unordered_map<sstring, unsigned int> ciphers = {
+            { "AES/CBC/PKCSU", 128 },
+            { "AES/ECB/Gris", 128 },
+            { "DES/CBC/PKCS12Padding", 56 },
+            { "DES/CBC/KorvPadding", 56 },
+            { "DES/CBC/MUPadding", 56 },
+    };
+    for (auto& p : ciphers) {
+        try {
+            key_info info{p.first, p.second};
+            symmetric_key k(info);
+            BOOST_ERROR("should not reach");
+        } catch (...) {
+            // ok.
+        }
+    }
+    return make_ready_future<>();
+}
+
+SEASTAR_TEST_CASE(test_valid_padding_options) {
+    static const std::unordered_map<sstring, unsigned int> ciphers = {
+            { "AES/CBC/PKCS", 128 },
+            { "AES/CBC/PKCSPadding", 128 },
+            { "AES/ECB/PKCS7Padding", 128 },
+            { "AES/ECB/PKCS7", 128 },
+            { "DES/CBC/PKCS5Padding", 56 },
+            { "DES/CBC/PKCS5", 56 },
+            { "AES/CBC/NoPadding", 128 },
+            { "AES/ECB/NoPadding", 128 },
+            { "DES/CBC/NoPadding", 56 },
+            { "AES/CBC/No", 128 },
+            { "AES/ECB/No", 128 },
+            { "DES/CBC/No", 56 },
+    };
+    for (auto& p : ciphers) {
+        key_info info{p.first, p.second};
+        symmetric_key k(info);
+    }
+    return make_ready_future<>();
+}
+
+/**
+ * Verifies that when using defaults in a key, the key info returned is still
+ * equal to the input one (by bit and textually)
+ */
+SEASTAR_TEST_CASE(test_cipher_defaults) {
+    static const std::unordered_map<sstring, std::vector<unsigned int>> ciphers = {
+            { "AES/CBC/PKCS5Padding", { 128, 192, 256 } },
+            { "AES/ECB/PKCS5Padding", { 128, 192, 256 } },
+            { "DES/CBC/PKCS5Padding", { 56 } },
+            { "DESede/CBC/PKCS5Padding", { 112, 168 } },
+            { "Blowfish/CBC/PKCS5Padding", { 32, 64, 448 } },
+            { "RC2/CBC/PKCS5Padding", { 40, 41, 64, 67, 120, 128 } },
+    };
+
+    for (auto& p : ciphers) {
+        for (auto s : p.second) {
+            auto alg = p.first;
+            for (;;) {
+                key_info info{alg, s};
+                symmetric_key k(info);
+
+                BOOST_REQUIRE_EQUAL(info, k.info());
+                BOOST_REQUIRE_EQUAL(boost::lexical_cast<std::string>(info), boost::lexical_cast<std::string>(k.info()));
+
+                auto i = alg.find_last_of('/');
+                if (i != sstring::npos) {
+                    alg.resize(i);
+                    continue;
+                }
+                // also verify that whatever we say (or don't say), we get a blockmode
+                // -> iv len > 0
+                BOOST_CHECK_GT(k.iv_len(), 0);
+
+                break;
+            }
+        }
+    }
+    return make_ready_future<>();
+}
