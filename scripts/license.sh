0c5cba1b4338c4c81f3e7652f5de6487f10564c0
Avi Kivity <avi@scylladb.com>
2022-02-07 20:38:56 +0200

    scripts/license.sh: adjust for SPDX
    With the move to SPDX, licenses are just a single line and can
    be adjusted with sed.
    
    Do that, with the logic
    
        AGPL-3.0-or-later -> ScyllaDB-Proprietary
        (AGPL-3.0-or-later and Apache-2.0) -> (ScyllaDB-Proprietary and Apache-2.0)
    
    Closes #2104

--- scripts/license.sh
+++ scripts/license.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#
 git ls-files \
     | xargs ls -Fd \
     | grep -vE '\.db$|\.crc32$' \
-    | grep -v '^scripts/license.awk$' \
+    | grep -v '^scripts/license.sh$' \
     | grep -v '[/@]$' \
     | sed 's/\*$//' \
     | xargs file --mime-type --no-pad  | sed -n '/: text/ { s/: text.*$//; p}' \
-    | xargs awk -i inplace -f scripts/license.awk
+    | xargs sed -i "/SPDX-License-Identifier:/s/AGPL-3.0-or-later/ScyllaDB-Proprietary/"
