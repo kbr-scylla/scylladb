829aabdf58ab36666bae6196cebd5273e5a5c8d3
Kamil Braun <kbraun@scylladb.com>
2024-03-01 11:51:29 +0100

    recovery majority loss 5 srvs

--- test/topology_experimental_raft/test_topology_recovery_majority_loss.py
+++ test/topology_experimental_raft/test_topology_recovery_majority_loss.py
@@ -19,26 +19,34 @@ from test.topology.util import reconnect_driver, restart, enter_recovery_state, 
 @pytest.mark.asyncio
 @log_run_time
 async def test_topology_recovery_after_majority_loss(request, manager: ManagerClient):
-    servers = await manager.servers_add(3)
+    servers = await manager.servers_add(5)
     cql = manager.cql
     assert(cql)
 
     logging.info("Waiting until driver connects to every server")
     hosts = await wait_for_cql_and_get_hosts(cql, servers, time.time() + 60)
 
-    srv1, *others = servers
+    srv1, srv2, *others = servers
+    rec = [srv1, srv2]
 
-    logging.info(f"Killing all nodes except {srv1}")
+    logging.info(f"Killing all nodes except {rec}")
     await asyncio.gather(*(manager.server_stop_gracefully(srv.server_id) for srv in others))
 
-    logging.info(f"Entering recovery state on {srv1}")
-    host1 = next(h for h in hosts if h.address == srv1.ip_addr)
-    await enter_recovery_state(cql, host1)
-    await restart(manager, srv1)
+    for r in rec:
+        logging.info(f"Entering recovery state on {r}")
+        host1 = next(h for h in hosts if h.address == r.ip_addr)
+        await enter_recovery_state(cql, host1)
+        await restart(manager, r)
+
+    await manager.server_sees_other_server(srv1.ip_addr, srv2.ip_addr)
+    await manager.server_sees_other_server(srv2.ip_addr, srv1.ip_addr)
+
     cql = await reconnect_driver(manager)
 
     logging.info("Node restarted, waiting until driver connects")
     host1 = (await wait_for_cql_and_get_hosts(cql, [srv1], time.time() + 60))[0]
+    host2 = (await wait_for_cql_and_get_hosts(cql, [srv2], time.time() + 60))[0]
+    hs = [host1, host2]
 
     for i in range(len(others)):
         to_remove = others[i]
@@ -46,29 +54,32 @@ async def test_topology_recovery_after_majority_loss(request, manager: ManagerCl
         logging.info(f"Removing {to_remove} using {srv1} with ignore_dead: {ignore_dead_ips}")
         await manager.remove_node(srv1.server_id, to_remove.server_id, ignore_dead_ips)
 
-    logging.info(f"Deleting old Raft data and upgrade state on {host1} and restarting")
-    await delete_raft_topology_state(cql, host1)
-    await delete_raft_data_and_upgrade_state(cql, host1)
-    await restart(manager, srv1)
-    cql = await reconnect_driver(manager)
+    for r, h in zip(rec, hs):
+        logging.info(f"Deleting old Raft data and upgrade state on {h} and restarting")
+        await delete_raft_topology_state(cql, h)
+        await delete_raft_data_and_upgrade_state(cql, h)
+        await restart(manager, r)
+        cql = await reconnect_driver(manager)
 
     logging.info("Node restarted, waiting until driver connects")
-    host1 = (await wait_for_cql_and_get_hosts(cql, [srv1], time.time() + 60))[0]
+    await wait_for_cql_and_get_hosts(cql, rec, time.time() + 60)
 
     logging.info("Waiting until upgrade to raft schema finishes.")
-    await wait_until_schema_upgrade_finishes(cql, host1, time.time() + 60)
+    for h in hs:
+        await wait_until_schema_upgrade_finishes(cql, h, time.time() + 60)
 
     logging.info("Triggering upgrade to raft topology")
     await manager.api.upgrade_to_raft_topology(host1.address)
 
     logging.info("Waiting until upgrade to raft topology finishes")
-    await wait_until_topology_upgrade_finishes(manager, host1.address, time.time() + 60)
+    for h in hs:
+        await wait_until_topology_upgrade_finishes(manager, h.address, time.time() + 60)
 
     logging.info("Checking consistency of data in system.topology and system.cdc_generations_v3")
-    await check_system_topology_and_cdc_generations_v3_consistency(manager, [host1])
+    await check_system_topology_and_cdc_generations_v3_consistency(manager, hs)
 
     logging.info("Add two more nodes")
-    servers = [srv1] + await manager.servers_add(2)
+    servers = rec + await manager.servers_add(2)
     hosts = await wait_for_cql_and_get_hosts(cql, servers, time.time() + 60)
 
     logging.info("Checking consistency of data in system.topology and system.cdc_generations_v3")
